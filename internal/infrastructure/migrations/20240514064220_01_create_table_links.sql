661b1d1a40fcd59a762e77fe0c28f8b14e510ec0
cfif <s_filip@mail.ru>
2024-05-14 11:06:41 +0400

    ver07 for iter13

---
+++ internal/infrastructure/migrations/20240514064220_01_create_table_links.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS links(
+	link_key TEXT,
+	link_url TEXT UNIQUE NOT NULL
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS links;
+-- +goose StatementEnd
---
+++ internal/infrastructure/migrations/20240514070130_03_add_index_idx_url_into_links.sql
@@ -0,0 +1,10 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE UNIQUE INDEX IF NOT EXISTS idx_url 
+	ON links (link_url);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP INDEX IF EXISTS idx_url ON links;
+-- +goose StatementEnd
